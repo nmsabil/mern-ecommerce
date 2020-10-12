6dab38a1e3d049b817fe4f59c5d303462327d136
Sabil <sabilmdtv1@gmail.com>
2020-10-12 19:38:39 +0100

    brought redux state into home screen

--- frontend/src/reducers/productReducers.js
+++ frontend/src/reducers/productReducers.js
@@ -11,7 +11,7 @@ export const productListReducer = (state = { products: [] }, action) => {
     case PRODUCT_LIST_SUCCESS:
       return {
         loading: false,
-        product: action.payload,
+        products: action.payload,
       };
     case PRODUCT_LIST_FAIL:
       return {
