0954ed14da992d262e214d9d7deabb0894e13776
Sabil <sabilmdtv1@gmail.com>
2020-11-03 19:59:06 +0000

    admin dashboard delete product functionality added

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -22,14 +22,15 @@ const getProductById = asyncHandler(async (req, res) => {
   }
 });
 
-// @desc Delete a product
-// @route Delete /api/products/:id
-// @access private/admin
+// @desc    Delete a product
+// @route   DELETE /api/products/:id
+// @access  Private/Admin
 const deleteProduct = asyncHandler(async (req, res) => {
   const product = await Product.findById(req.params.id);
+
   if (product) {
     await product.remove();
-    res.json({ message: "product removed" });
+    res.json({ message: "Product removed" });
   } else {
     res.status(404);
     throw new Error("Product not found");
